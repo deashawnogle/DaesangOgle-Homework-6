1bbe5ab78b50e6008f73800030c58dcafc4aaf99
deashawnogle <deashawnogle@gmail.com>
2022-01-09 21:50:58 -0800

    create index.html with Bootstrap

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+const apiKey = "36b920f4ff2038fe3b14d0fc97c766c3";
+let resultArea = $('#resultArea');
+let result = $('#result');
+let history = localStorage.getItem("history");
+
+
+
+
+//"api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey
+
+
+//function for search button
+// $('#serachButton').click(function (event) {
+//     event.preventDefault()
+//     todayContent.text('')
+//     resultArea.text('')
+//     getCity()
+// })

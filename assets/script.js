b6872b3a63f5d4760d93b6026a08938f788a0594
deashawnogle <deashawnogle@gmail.com>
2022-01-17 19:33:34 -0800

    search button, get info from weather api

--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,110 @@
 //global vaiables
 const apiKey = "7ba4078f3e5e2434eebb3325598ec14b";
-const city = $('.search-input');
+const searchButton = $('#searchButton');
 const cities = localStorage.getItem("history") ? JSON.parse(localStorage.getItem("history")) : []
-const requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
+const fiveDayKey = "1221a358bdfdc0f4c7620820b753e607"
 const uvIndexApiKey = "e2ccdb6ba0e97a2de8ffa8f973f03002"
 
-//display weather current weather
-function disply(city = $("#search-input").val().trim()) {
+searchButton.on("click", function(){
+    const cityName = $("#search-input").val().trim()
+    display(cityName)
+});
+
+function display(cityName) {
+    
+    let requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
 
     $.ajax({
         url: requestURL,
         method: "GET"
     }).then(function(response) {
 
+
         let iconCode = response.weather[0].icon;
-        let iconUrl = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png"
-    
+        let iconUrl = "http://openweathermap.org/img/wn/"+ iconCode + "@2x.png"
+
         $(".name").text(response.name);
         $(".date").text(new Date(response.dt*1000).toLocaleDateString());
-        $(".date").attr('src', iconUrl);
-        $(".temperature").text("Temperature (F): " + response.main.temp + "F");
+        $(".img").attr('src', iconUrl);
+        $(".temp").text("Temperature (F): " + response.main.temp + "F");
         $(".humid").text("Humidity: " + response.main.humidity + " %");
         $(".wind").text("Wind Speed: " + response.wind.speed + " MPH");        
-        
+
+        let lat = response.coord.lat
+        let lon = response.coord.lon
+
+        fivedays(lat,lon);
     });
 
 }
 
+function fivedays (lat,lon) {
+    let requestURL5 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
 
-function historyButtons() {
-    $("#history").empty();
+    $.ajax({
+        url: requestURL5,
+        method: "GET"
+    }).then(function(response) {
 
-    for (var i = 0; i < cities.length; i++) {
-    let add = $("<button>");
-    add.addClass("city");
-    add.attr("data-name", cities[i]);
-    add.text(cities[i]);
-    $("#history").append(add);
-    }
-};
+        console.log (response)
+        for (let i = 1; i < response.daily.length - 2; i++) {
 
-$("#search-city").on("click", function(event) {
-    event.preventDefault();
-    let city = $("#searchButton").val().trim();
-    cities.push(city);
-    localStorage.setItem("history", JSON.stringify(cities))
-    historyButtons();
-    display();
+        let iconCode = response.daily[i].weather[0].icon;
+        let iconUrl = "http://openweathermap.org/img/wn/"+ iconCode + "@2x.png"
+
+        $(".date" + i).text(new Date(response.daily[i].dt*1000).toLocaleDateString());
+        $(".dateimg" + i).attr('src', iconUrl);
+        $(".dateTemp" + i).text("Temperature (F): " + response.daily[i].temp.day + "F");
+        $(".dateHumid" + i).text("Humidity: " + response.daily[i].humidity + " %");
+        $(".dateWind" + i).text("Wind Speed: " + response.daily[i].wind_speed + " MPH");   
+    };
 });
+}
 
-$(document).on("click", ".city", (e)=>display(e.target.textContent));
 
-historyButtons();
+
+
+// function history() {
+//     console.log("button clicked")
+//     $("#history").empty();
+
+//     for (var i = 0; i < cities.length; i++) {
+//     let add = $("<button>");
+//     add.addClass("city");
+//     add.attr("data-name", cities[i]);
+//     add.text(cities[i]);
+//     $("#history").append(add);
+//     }
+// };
+
+// $("#search-city").on("click", function(event) {
+//     event.preventDefault();
+//     let city = $("#searchButton").val().trim();
+//     cities.push(city);
+//     localStorage.setItem("history", JSON.stringify(cities))
+//     historyButtons();
+//     display();
+// });
+
+// $(document).on("click", ".city", (e)=>display(e.target.textContent));
+
+// searchButtons();
+
+//display weather current weather
+
+
+
+
+
+//function uvIndex (lon, lat)
+
+//let uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid="+ apiKey + "&lat=" + lat + "&lon=" + lon;
+
+
+
+// $(document).on("click", ".city", (e)=>display(e.target.textContent));
+
+// historyButtons();
 
 
 // function GetInfo () {

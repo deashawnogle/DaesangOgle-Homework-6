bb921911f13935df2015d10d8a10c2f70acc1eff
deashawnogle <deashawnogle@gmail.com>
2022-01-19 19:49:08 -0800

    UVIndex function added

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,16 @@
 const apiKey = "7ba4078f3e5e2434eebb3325598ec14b";
 const searchButton = $('#searchButton');
 const cities = localStorage.getItem("history") ? JSON.parse(localStorage.getItem("history")) : []
-const fiveDayKey = "1221a358bdfdc0f4c7620820b753e607"
-const uvIndexApiKey = "e2ccdb6ba0e97a2de8ffa8f973f03002"
 
+
+//function for Searh click / 
 searchButton.on("click", function(){
     const cityName = $("#search-input").val().trim()
     display(cityName)
 });
 
+
+//Display current weather page from API with AJAX
 function display(cityName) {
     
     let requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
@@ -19,25 +21,44 @@ function display(cityName) {
         method: "GET"
     }).then(function(response) {
 
-
+        //console.log(response)
         let iconCode = response.weather[0].icon;
-        let iconUrl = "http://openweathermap.org/img/wn/"+ iconCode + "@2x.png"
+        let iconUrl = "http://openweathermap.org/img/wn/"+ iconCode + "@2x.png";
 
         $(".name").text(response.name);
         $(".date").text(new Date(response.dt*1000).toLocaleDateString());
         $(".img").attr('src', iconUrl);
-        $(".temp").text("Temperature (F): " + response.main.temp + "F");
-        $(".humid").text("Humidity: " + response.main.humidity + " %");
-        $(".wind").text("Wind Speed: " + response.wind.speed + " MPH");        
+        $(".temp").text("Temp (F): " + response.main.temp + "F");
+        $(".humid").text("Humid: " + response.main.humidity + " %");
+        $(".wind").text("Wind: " + response.wind.speed + " MPH");        
+        
 
-        let lat = response.coord.lat
-        let lon = response.coord.lon
+        const lat = response.coord.lat
+        const lon = response.coord.lon
 
         fivedays(lat,lon);
     });
 
 }
 
+//Receiving UV index(lat,lon) using by ajax  --need to be fix
+function uvIndex(lat, lon) {
+
+    let uvURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=daily&appid=" + apiKey + "&units=imperial"
+
+    console.log(response)
+
+    $.ajax({
+        url: uvURL,
+        method: "GET"
+    }).then(function(response){
+
+        $(".uv").text("UV Index: " + response.current.uvi);
+
+    });
+}
+
+//Display fivdays forcast by ajax  --Icon is not displaying
 function fivedays (lat,lon) {
     let requestURL5 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
 
@@ -46,17 +67,17 @@ function fivedays (lat,lon) {
         method: "GET"
     }).then(function(response) {
 
-        console.log (response)
+        console.log(response)
         for (let i = 1; i < response.daily.length - 2; i++) {
 
         let iconCode = response.daily[i].weather[0].icon;
-        let iconUrl = "http://openweathermap.org/img/wn/"+ iconCode + "@2x.png"
+        let iconUrl = "http://openweathermap.org/img/wn/"+ iconCode + ".png";
 
         $(".date" + i).text(new Date(response.daily[i].dt*1000).toLocaleDateString());
         $(".dateimg" + i).attr('src', iconUrl);
-        $(".dateTemp" + i).text("Temperature (F): " + response.daily[i].temp.day + "F");
-        $(".dateHumid" + i).text("Humidity: " + response.daily[i].humidity + " %");
-        $(".dateWind" + i).text("Wind Speed: " + response.daily[i].wind_speed + " MPH");   
+        $(".dateTemp" + i).text("Temp (F): " + response.daily[i].temp.day + "F");
+        $(".dateHumid" + i).text("Humid: " + response.daily[i].humidity + " %");
+        $(".dateWind" + i).text("Wind: " + response.daily[i].wind_speed + " MPH");   
     };
 });
 }
